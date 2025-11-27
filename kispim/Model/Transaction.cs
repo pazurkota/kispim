6d3c35f06ae8feec6e35c0e594fba7f787ed9557
Piotr Jelonek <piotrek.jelonek@icloud.com>
2025-11-27 22:04:44 +0100

    Adds Transaction model

--- kispim/Model/Transaction.cs
+++ kispim/Model/Transaction.cs
@@ -1,6 +1,25 @@
-﻿namespace kispim.Model;
+﻿using SQLite;
 
+namespace kispim.Model;
+
+[Table("transactions")]
 public class Transaction
 {
+    [PrimaryKey]
+    [AutoIncrement]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")] 
+    public string Name { get; set; } = null!;
     
+    [Column("amount")]
+    public decimal Amount { get; set; }
+    
+    // public CurrencyType Currency { get; set; }
+    
+    [Column("date")]
+    public DateTime Date { get; set; }
+    
+    // public Tag[] Tags { get; set; }
 }
