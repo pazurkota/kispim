45a20d9ba8b58a85cd0c8c3817bad87541464fcd
Piotr Jelonek <piotrek.jelonek@icloud.com>
2025-11-28 16:31:44 +0100

    Created ViewModel for TransactionPage

---
+++ kispim/ViewModel/TransactionPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace kispim.ViewModel;
+
+public class TransactionPageViewModel : ObservableObject
+{
+    public TransactionPageViewModel()
+    {
+        
+    }
+}
